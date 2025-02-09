9fe2491cdc82bfb44c1f05755a85d5bdd23e71ec
Jerome Sabidong <jeromesabidong@gmail.com>
2025-02-09 14:21:23 +0900

    day 26

--- resources/views/components/authenticated-layout.blade.php
+++ resources/views/components/authenticated-layout.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+
+    @vite(['resources/js/app.jsx'])
 </head>
 
 <body class="h-full">
--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -8,7 +8,7 @@
             <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200">
                 <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name  }}</div>
                 <div>
-                    <b>{{ $job['title'] }}</b>: Pays {{ $job['salary'] }} per year.
+                    <b class="text-laracasts">{{ $job['title'] }}</b>: Pays {{ $job['salary'] }} per year.
                 </div>
             </a>
         @endforeach
